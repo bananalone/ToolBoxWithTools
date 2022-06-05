d3bc263a3d9e44bacc25de24b8733bb20fb2de9f
bananalone <yx243354200@qq.com>
2022-06-05 11:21:30 +0800

    fix bug VideoDownloader can not display progressbar

--- src/plugins/VideoDownloader/downloaders/utils.py
+++ src/plugins/VideoDownloader/downloaders/utils.py
@@ -11,11 +11,12 @@ def merge_audio_video(audio: str, video: str, output: str):
     
     
 def download_media(url: str, headers: dict, output: str, chunk_size = 2048):
-    response = requests.get(url, headers=headers, stream=True)
-    with open(output, 'wb') as f:
-        for chunk in tqdm(response.iter_content(chunk_size=chunk_size)):
-            if chunk:
-                f.write(chunk)
+    with requests.get(url, headers=headers, stream=True) as response:
+        length = int(response.headers['content-length'])
+        with open(output, 'wb') as f:
+            for chunk in tqdm(response.iter_content(chunk_size=chunk_size), total=(length+chunk_size-1)//chunk_size):
+                if chunk:
+                    f.write(chunk)
                 
                 
 def removeSubStr(text: str, *subs) -> str:
